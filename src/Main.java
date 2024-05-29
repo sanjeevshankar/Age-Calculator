dfdf0a95fc0838d7c5d2f1dc4ffe83c82988965e
sanjeevshankar <sanjeevshankar2007@gmail.com>
2024-05-29 09:21:18 -0400

    Name Request

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 import utils.age.AgeImplementation;
+import utils.name.NameImplementation;
 
 import static utils.age.AgeConstant.BIG_Y;
 import static utils.age.AgeConstant.SMALL_Y;
 
-
 public class Main {
     public static void main(String[] args) {
         AgeImplementation ageImplementation = new AgeImplementation();
+        NameImplementation nameImplementation = new NameImplementation();
         String proceed = SMALL_Y;
         while (proceed.equals(SMALL_Y) || proceed.equals(BIG_Y)) {
-            ageImplementation.validateAge(ageImplementation.getAge());
+            nameImplementation.getFirstName();
+            nameImplementation.getLastName();
+            int age = ageImplementation.getAge();
+            nameImplementation.printName();
+            ageImplementation.validateAge(age);
             proceed = ageImplementation.getUserInput();
         }
     }
---
+++ src/utils/name/NameRequest.java
@@ -0,0 +1,11 @@
+package utils.name;
+
+public interface NameRequest {
+
+    String getFirstName();
+    String getLastName();
+    void printName();
+
+
+}
+
